d3f63773a9e13b00f0696c524862b5ae232a161f
Jesse Alford <jalford@pivotallabs.com>
2014-05-20 10:58:59 -0700

    sets app name in manifest
    Signed-off-by: Jesse Alford <jalford@pivotallabs.com>

--- interrogator.sh
+++ interrogator.sh
@@ -10,7 +10,7 @@ set -x
 cf --version
 
 # Set CF to use a seperate config file 
-export CF_HOME=/tmp/
+export CF_HOME=/tmp/interrogator
 
 # Login and target the app's space
 cf api api.staging.cf-app.com
@@ -18,4 +18,14 @@ cf auth interrogator interrogator
 cf target -o jesse -s jesse
 
 # Get the app's name, instances, quota, and routes
-# cf app dora
+# and store them as a text file
+cf app dora > /tmp/interrogator/interrogator_cf_app.txt
+
+# Assign them to env vars
+INTERROGATOR_APP_NAME='dora'
+INTERROGATOR_MANIFEST=/tmp/interrogator/$INTERROGATOR_APP_NAME-manifest.yml
+
+# Initialize a manifest file and write the app details to it
+rm $INTERROGATOR_MANIFEST
+echo '---' >> $INTERROGATOR_MANIFEST
+echo "name: $INTERROGATOR_APP_NAME" >> $INTERROGATOR_MANIFEST
